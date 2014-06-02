8bd3a7657bf70838ffbba0f00be5bc913f4fc73e
Jan <janie177@hotmail.com>
2014-06-02 08:23:08 +0200

    Update SquidRain.java

--- src/main/java/com/minegusta/mgbosses/powers/abilities/SquidRain.java
+++ src/main/java/com/minegusta/mgbosses/powers/abilities/SquidRain.java
@@ -7,6 +7,20 @@ public class SquidRain implements Ability{
 
     @Override
     public void run(Player p, LivingEntity entity, double damage) {
+        
+        final Location target = p.getLocation();
+        location.add(0,20,0);
+        final World world = p.getWorld();
+        for (int i = 0; i < 21; i++) {
+                    Bukkit.getScheduler().scheduleSyncDelayedTask(MausWasHere.PLUGIN, new Runnable() {
+
+                        @Override
+                        public void run() {
+							world.spawnEntity(location, EntityType.SQUID);
+							p.sendMessage(ChatColor.RED + "[" + entity.getCustomName() + ChatColor.RED + "] " + ChatColor.RESET + "I'm even better than Aquaman!");
+                        }
+
+                    }, i);
 
     }
 }
