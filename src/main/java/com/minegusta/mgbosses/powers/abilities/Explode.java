31282df0fffa0f83b85c2582adcc8ffd7855b381
Jan <janie177@hotmail.com>
2014-06-02 08:44:42 +0200

    Added Explode
    The method for the explosion might be non-existant. Check this later!

--- src/main/java/com/minegusta/mgbosses/powers/abilities/Explode.java
+++ src/main/java/com/minegusta/mgbosses/powers/abilities/Explode.java
@@ -7,7 +7,12 @@ import org.bukkit.entity.Player;
 public class Explode implements Ability{
 
     @Override
-    public void run(Player p, LivingEntity entity, double damage) {
+    public void run(Player p, LivingEntity entity, double damage) 
+    {
+        p.sendMessage(ChatColor.RED + "[" + entity.getCustomName() + ChatColor.RED + "] " + ChatColor.RESET + "BOOM!");
+
+        //TODO Not sure if this will work
+        p.getLocation().createExplosion(Explosion.NORMAL, false);
 
     }
 }
