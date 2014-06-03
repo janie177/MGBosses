692e03cd6c9f470597e7df73bd2d0e20a57371e2
Jan <janie177@hotmail.com>
2014-06-03 11:20:46 +0200

    Adjusted time
    Now runs particles 4 times a second instead of 20 to reduce lagg. Also fixed effect duration.

--- src/main/java/com/minegusta/mgbosses/powers/abilities/SmokeScreen.java
+++ src/main/java/com/minegusta/mgbosses/powers/abilities/SmokeScreen.java
@@ -10,17 +10,19 @@ public class SmokeScreen implements Ability{
     @Override
     public void run(Player player, LivingEntity ent, double damage) {
 
+        player.sendMessage(ChatColor.RED + "[" + ent.getCustomName() + ChatColor.RED + "] " + ChatColor.RESET + "Shadow is on my side " + p.getDisplayName() + ChatColor.RESET + "...");
+
         final Player p = player;
         final LivingEntity entity = ent;
         final Location target = entity.getLocation();
         final World world = p.getWorld();
-        for (int i = 0; i < 21; i++) {
+        for (int i = 0; i < 20 * 12; i++) {
+            final int k = i;
             Bukkit.getScheduler().scheduleSyncDelayedTask(Main.PLUGIN, new Runnable() {
 
                 @Override
                 public void run() {
-                    world.spigot().playEffect(target, Effect.POTION_SWIRL, 0, 0, 9, 2, 9, 1, 30, 25);
-                    p.sendMessage(ChatColor.RED + "[" + entity.getCustomName() + ChatColor.RED + "] " + ChatColor.RESET + "Shadow is on my side " + p.getDisplayName() + ChatColor.RESET + "...");
+                    if(k % 5 == 0)world.spigot().playEffect(target, Effect.POTION_SWIRL, 0, 0, 9, 2, 9, 1, 30, 25);
                 }
 
             }, i);
